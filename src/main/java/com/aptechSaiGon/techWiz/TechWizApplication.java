5070ebd74a89eba23fc6b9908df722832dd4b849
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-08-09 11:34:50 +0700

    initialize

---
+++ src/main/java/com/aptechSaiGon/techWiz/TechWizApplication.java
@@ -0,0 +1,13 @@
+package com.aptechSaiGon.techWiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechWizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechWizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aptechSaiGon/techWiz/TechWizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aptechSaiGon.techWiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechWizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
